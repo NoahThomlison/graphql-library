06da0e942ab364223caf75297545636731e4b632
noahthomlison <noahthomlison@gmail.com>
2022-05-18 19:49:53 -0700

    adds graphQL middleware endpoint

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
+const graphQLHTTP = require('express-graphql')
 
+app.use("/graphQL", graphQLHTTP({
+  
+}))
 
 app.listen(4000, () => {
   console.log("App listening on port 400")
