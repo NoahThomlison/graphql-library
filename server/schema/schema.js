278c07bf22d3fc50ab81c6388467799e5fb0ce57
noahthomlison <noahthomlison@gmail.com>
2022-05-19 20:04:54 -0700

    adds more testsbooks, adds relationship between authors to books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,13 +6,20 @@ const {
   GraphQLString, 
   GraphQLSchema, 
   GraphQLID, 
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql
 
 const testBooks = [
   {name: "Name of the wind", genre: "fantasy", id:"1", authorid: "1"},
   {name: "The something game", genre: "fantasy", id:"2", authorid: "2"},
-  {name: "Battle of Alberta", genre: "fantasy", id:"3", authorid: "3"}
+  {name: "Battle of Alberta", genre: "fantasy", id:"3", authorid: "3"},
+  {name: "Stuff I like", genre: "fantasy", id:"1", authorid: "1"},
+  {name: "This is a phone?", genre: "fantasy", id:"2", authorid: "2"},
+  {name: "Pills Pills Pills", genre: "fantasy", id:"3", authorid: "3"},
+  {name: "Cat in the hat", genre: "fantasy", id:"1", authorid: "1"},
+  {name: "SodaStream Wars", genre: "fantasy", id:"2", authorid: "2"},
+  {name: "Where did my hair go?", genre: "fantasy", id:"3", authorid: "3"},
 ]
 
 const testAuthors = [
@@ -41,7 +48,13 @@ const AuthorType = new GraphQLObjectType({
   fields: ()=>({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
-    age: {type: GraphQLInt}
+    age: {type: GraphQLInt},
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(testBooks, {authorid: parent.id})
+      }
+    }
   })
 })
 
