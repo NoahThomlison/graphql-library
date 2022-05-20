77458829f07cf4a511e43e9eb5d0531edadddc42
noahthomlison <noahthomlison@gmail.com>
2022-05-19 21:23:47 -0700

    adds connection to mongoDB database

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ const express = require("express")
 const app = express()
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema")
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://noahthomlison:test123@cluster0.zvasa.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connection.once("open", () => {
+  console.log("connected to database")
+})
 
 app.use('/graphql', graphqlHTTP({
   schema:schema,
