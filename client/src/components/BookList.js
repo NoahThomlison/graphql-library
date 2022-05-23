b74b6d901178efa9b294c56e734eb341c3b6f88d
noahthomlison <noahthomlison@gmail.com>
2022-05-23 14:21:02 -0700

    adds code to retrieve books from server

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,6 +1,22 @@
 import Book from './Book.js'
+import {
+  gql,
+  useQuery,
+} from "@apollo/client";
+
+const getBookQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
 
 function BookList() {
+  const { loading, error, data } = useQuery(getBookQuery);
+    console.log(data);
+
   return (
     <div className="BookList">
       <h1>BookList</h1>
