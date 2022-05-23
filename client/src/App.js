558ff1c8a5079fcdf526e642e8f5501221a3672d
noahthomlison <noahthomlison@gmail.com>
2022-05-23 14:17:06 -0700

    moves appolo info to index from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import BookList from './components/BookList'
 import React, {useState, useEffect} from "react"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 function App() {
   return (
-    <div className="App">
-      <BookList></BookList>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <BookList></BookList>
+      </div>
+    </ApolloProvider>
   );
 }
 
