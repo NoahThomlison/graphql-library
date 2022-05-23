7c751d3db758cf19ef25d3f5b067c5b1079c5e65
noahthomlison <noahthomlison@gmail.com>
2022-05-22 19:21:35 -0700

    adds useEffect and useState

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: 'mongodb+srv://noahthomlison:test123@cluster0.zvasa.mongodb.net/?retryWrites=true&w=majority',
+  cache: new InMemoryCache()
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
