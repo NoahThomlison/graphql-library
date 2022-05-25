9a1de2909d64e9aae33ad76fea7c749e1b1f37c7
noahthomlison <noahthomlison@gmail.com>
2022-05-24 19:24:28 -0700

    adds controls componets with useState for the book name and genre

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,23 +1,27 @@
 import {
   useQuery,
 } from "@apollo/client";
-
 import {getAuthors} from '../queries/queries'
+import React, {useState, useEffect} from "react"
 
 function AddBook() {
+  const [name, setName] = useState("")
+  const [genre, setGenre] = useState("")
+
   const { data } = useQuery(getAuthors);
+
   console.log(data)
   if (data) return (
     <form id="add-book">
 
     <div className="field">
       <label>Book name:</label>
-      <input type="text"/>
+      <input type="text" onChange={event => setName(event.target.value)} value={name}/>
     </div>
 
     <div className="field">
       <label>Genre:</label>
-      <input type="text"/>
+      <input type="text" onChange={event => setGenre(event.target.value)} value={genre}/>
     </div>
 
     <div className="field">
