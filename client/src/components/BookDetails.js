ab0de1cee35f54bb18d77f40c0f6de105d1dc1c8
noahthomlison <noahthomlison@gmail.com>
2022-05-26 08:22:18 -0700

    adds code to bookDetails to show appropriate data

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -8,19 +8,29 @@ function BookDetails({id}) {
 	const { loading, error, data } = useQuery(getBookDetailsQuery, {
 		variables: { id: id }
 	});  
-  if (loading) return(<h1>Loading</h1>)
+  if (loading) return(<h5>Loading</h5>)
   if (error) {
     console.log(error)
   }
+  if (!data) {
+    return(
+      <h5>No Book Selected</h5>
+    )
+  }
   if (data) {
     console.log(data)
-    return(<h1>data</h1>)
-  }
-
-  return (
-    <div className="BookDetails">
-    </div>
-  );
+    return(
+      <div className="bookDetails">
+        <h4>{data.book.name}</h4>
+        <h5>Genre: {data.book.genre}</h5>
+        <h5>By: {data.book.author.name}</h5>
+        <h5>Age: {data.book.author.age}</h5>
+        <h5>Other Books by this Author</h5>
+        <div className="booksByAuthor">
+          {data.book.author.books.map((book) => {return(<li>{book.name}</li>)})}
+        </div>
+      </div>
+    )}
 }
 
 export default BookDetails;
